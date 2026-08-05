9b55fecc1a212cb9c2a9a7c26aa164adf57ff49a
Trulioo Release Automation <release-automation@trulioo.com>
2026-08-05 16:58:07 -0700

    Release 3.2.2-beta.0

--- Package.swift
+++ Package.swift
@@ -13,15 +13,15 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Trulioo/kyc-documents-capture.git", exact: "3.2.1"),
-        .package(url: "https://github.com/Trulioo/trulioo-ios.git", exact: "3.2.1"),
+        .package(url: "https://github.com/Trulioo/kyc-documents-capture.git", exact: "3.2.2-beta.0"),
+        .package(url: "https://github.com/Trulioo/trulioo-ios.git", exact: "3.2.2-beta.0"),
         .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.5.2"),
     ],
     targets: [
         .binaryTarget(
             name: "TruliooKYCDocuments",
-            url: "https://github.com/Trulioo/kyc-documents/releases/download/3.2.1/TruliooKYCDocuments.xcframework.zip",
-            checksum: "6d949f4ce1446500a3ec8a166c76dc97f3a1822a547f82f3ba024076b2224f4e"
+            url: "https://github.com/Trulioo/kyc-documents/releases/download/3.2.2-beta.0/TruliooKYCDocuments.xcframework.zip",
+            checksum: "87c37791c1d037ab3e13f606145a95fe4370dee450ee50cabdc5fc744640556a"
         ),
         .target(
             name: "TruliooKYCDocumentsDependencies",
